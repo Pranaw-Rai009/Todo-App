084a9c40485c91a4d6636ca4159fbb0d0d46d67a
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-08-03 16:19:58 +0545

    feat: user update

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-import {createUser, getUserById, getAllUsers, deleteUserById} from '../controllers/users.controllers.js'
+import {createUser, getUserById, getAllUsers, deleteUserById, udpateUserById} from '../controllers/users.controllers.js'
 
 const router = Router()
 
-router.post("/register", createUser)
-router.get("/search/:id", getUserById)
-router.get("/list", getAllUsers)
-router.delete("/", deleteUserById)
+router.post("/registerUser", createUser)
+router.get("/searchUser/:id", getUserById)
+router.get("/listUser", getAllUsers)
+router.delete("/deleteUser", deleteUserById)
+router.patch("/updateUser/:id", udpateUserById)
 
 export default router
