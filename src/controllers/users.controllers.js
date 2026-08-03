aad58661df65ad32722c2cf3e0d7f27c2b7326bb
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-08-03 13:38:10 +0545

    feat: Added users creation

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -9,7 +9,43 @@ export const createUser = async (req, res) => {
             password
         })
         res.status(201).json(user)
+        console.log("User created successfully")
     } catch(error) {
+        console.log(error)
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export const getAllUsers = async(req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+export const getUserById = async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export const deleteUserById = async(req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        console.log("User deleted successfully")
+    } catch (error) {
         console.log(error)
         res.status(500).json({
             message: error.message
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,11 @@
 import {Router} from 'express'
-import {createUser} from '../controllers/users.controllers.js'
+import {createUser, getUserById, getAllUsers, deleteUserById} from '../controllers/users.controllers.js'
 
 const router = Router()
 
 router.post("/register", createUser)
+router.get("/search/:id", getUserById)
+router.get("/list", getAllUsers)
+router.delete("/", deleteUserById)
+
 export default router
